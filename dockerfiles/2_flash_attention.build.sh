d40e0e99968f4efd9380d88d397fdb75fe0374aa
Calvin Vette <calvinvette@gmail.com>
2025-08-14 18:46:09 -0400

    Dockerfile.amd64: Removed flash_attn compile dockerfiles/*flash_attn* - Dockerfile just to build flash_attn and script to copy it to out

--- dockerfiles/2_flash_attention.build.sh
+++ dockerfiles/2_flash_attention.build.sh
@@ -2,9 +2,22 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright (c) 2025 Calvin Vette
 
+# TODO
+# There has to be a cleaner way of doing this...
+
 mkdir -p out
 
 docker build . \
     -f Dockerfile.flash-attention.amd64 \
-    -v ./out:/out \
-    -t nge/builder.flash-attention
+    -t nge/builder.flash-attention \
+    && \
+docker run \
+        --rm \
+        --gpus all \
+        --runtime=nvidia \
+        --ipc=host \
+        --network host \
+        -it \
+        -v ./out:/out \
+        --name booger nge/builder.flash-attention \
+        cp /workspace/flash-attention/dist/flash_attn-2.8.2-cp310-cp310-linux_x86_64.whl /out/
